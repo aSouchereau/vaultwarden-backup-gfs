c95406fef309d72f75f570713c228d0266d332f7
aSouchereau <alex@souch.ca>
2023-10-17 21:09:17 -0400

    Added option for running populate output script

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # populate output for dev testing
-cd /app/scripts/tests
-sh cleanup_test.sh DELETEMYDATA
+if [ "$APP_ENV" = "dev" ] && [ "$POPULATE_OUTPUT_ON_START" = true ]; then
+    cd /app/scripts/tests
+    sh cleanup_test.sh DELETEMYDATA
+fi
 
 # start cron
 /usr/sbin/crond -f -l 8
