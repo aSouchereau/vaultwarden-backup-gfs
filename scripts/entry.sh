58ce5738af999a87680c64c3397d00db251a201b
aSouchereau <alex@souch.ca>
2024-02-04 21:49:52 -0500

    Refactored to avoid potential confusion

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -16,9 +16,9 @@ echo " ╚╝ ╚╩╝       ╚═╝╩ ╩╚═╝╩ ╩╚═╝╩      
 
 
 # populate output for dev testing
-if [ "$APP_ENV" = "dev" ] && [ "$POPULATE_OUTPUT_ON_START" = true ]; then
+if [ "$APP_ENV" = "dev" ] && [ "$CREATE_DUMMY_FILES" = true ]; then
     cd /app/scripts/dev
-    sh populate_output.sh DELETEMYDATA
+    sh create_dummy_files.sh NUKEMYBACKUPS
 fi
 
 # load correct cron schedules depending on environment
